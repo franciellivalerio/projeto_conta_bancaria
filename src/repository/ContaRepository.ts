00d3796384b1b064c7917b109880d0eeb09d2123
franciellivalerio <franciellivaleriodeoliveira@gmail.com>
2024-07-08 13:30:14 -0300

    Criação da Conta Repository.

---
+++ src/repository/ContaRepository.ts
@@ -0,0 +1,19 @@
+import { conta } from "../model/Conta";
+
+export interface ContaRepository{
+
+    // Métodos do CRUD
+
+    procurarPorNumero(numero: number): void;
+    listarTodas(): void;
+    cadastrar(conta: conta): void;
+    atualizar(conta: conta): void;
+    deletar(numero: number): void;
+
+    // Métodos Bancários
+
+    sacar(numero: number, valor: number): void
+    depositar(numero: number, valor: number): void
+    transferir(numeroOrigem: number, numeroDestino: number, valor: number): void
+
+}
